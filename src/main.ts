a99f08b4fc0ffbd97ea8212f6256c5d2fcc21dbc
David Castillo <davecas26@gmail.com>
2024-08-16 09:32:20 -0400

    Implemement ValidationPipe on all app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Cleans the object we receive from unneeded garbage properties
+      whitelist: true,
+      // Returns that values do not exist, within what is expected in the body.
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
   console.log('Listen on http://localhost:3000/');
 }
